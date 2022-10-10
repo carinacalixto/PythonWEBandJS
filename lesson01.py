a8941cf0bcee9473b323e92962c7b19e1c0f773e
Carina Calixto Ribeiro de Araujo <carinacalixto@gmail.com>
2022-10-10 18:24:25 -0300

    lesson01 - print

---
+++ lesson01.py
@@ -0,0 +1,5 @@
+#Change Codecademy to your name in the script to the right. Run the code to see what it does!
+
+# my_name = "Codecademy"
+my_name = "Carina Calixto"
+print("Hello and welcome " + my_name + "!")
