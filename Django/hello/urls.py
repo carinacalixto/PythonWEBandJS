d3ccdd7a46c44ca189bdf72a8e3926601375fb0b
Carina Calixto Ribeiro de Araujo <carinacalixto@gmail.com>
2022-10-18 15:10:50 -0300

    Lecture 3 Django pt2

--- Django/hello/urls.py
+++ Django/hello/urls.py
@@ -2,6 +2,8 @@ from unicodedata import name
 from django.urls import path
 from . import views
 
+app_name = "hello"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("carina", views.carina, name="carina"),
